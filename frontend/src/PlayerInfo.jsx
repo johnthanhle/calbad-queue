070180c5610d1b40ae6079228b70c787b1d70f59
Aster Chun <asterchun@berkeley.edu>
2020-12-19 15:37:50 -0800

    updating playerinfo

---
+++ frontend/src/PlayerInfo.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { TextField } from "@material-ui/core";
+import { Edit, Check } from "@material-ui/icons";
+
+const UserInfo = props => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [userName, setUserName] = useState(props.user.name);
+  const [userName2, setUserName2] = useState(props.user.name2)
+
+  const handleTextboxUpdate = event => {
+    setUserName(event.target.value);
+  };
+
+  const handleTextboxUpdate2 = even => {
+    setUserName2(event.target.value)
+  };
+
+  const save = () => {
+    const newUser = {};
+    Object.assign(newUser, props.user);
+    newUser.name = userName;
+    props.updateFunction(newUser);
+    setIsEdit(false);
+  };
+
+  return (
+    <div>
+      {!isEdit && (
+        <h4 onClick={() => setIsEdit(true)}>
+          {props.user.name} (uid: {props.user.uid}) <Edit fontSize="small" />
+        </h4>
+      )}
+      {isEdit && (
+        <div>
+          <TextField
+            value={userName === props.defaultUser.name ? "" : userName}
+            onChange={handleTextboxUpdate}
+            onBlur={save}
+            label="Name"
+            variant="outlined"
+            size="small"
+          />
+          <TextField
+            value={userName2 === props.defaultUser.name ? "" : userName}
+            onChange={handleTextboxUpdate}
+            onBlur={save}
+            label="Partner Name"
+            variant="outlined"
+            size="small"
+          />
+          <Check onClick={save} />
+        </div>
+      )}
+    </div>
+
+  );
+};
+
+export default UserInfo;
