0281870a0afc87e2247c35adde6f5136e9662982
Aster Chun <asterchun@berkeley.edu>
2020-12-19 16:21:44 -0800

    added a space in title

--- frontend/src/PlayerView.jsx
+++ frontend/src/PlayerView.jsx
@@ -21,7 +21,7 @@ const PlayerView = props => {
 
   return (
     <Container maxWidth="sm">
-      <h1>Cal Badminton Open GymQueue</h1>
+      <h1>Cal Badminton Open Gym Queue</h1>
       {user && (
         <PlayerInfo
           user={user}
