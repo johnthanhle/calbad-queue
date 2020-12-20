425fdc03e8df558fbf42a17597452baba197b28e
Aster Chun <asterchun@berkeley.edu>
2020-12-19 16:21:01 -0800

    player view maybe ready?

--- frontend/src/PlayerView.jsx
+++ frontend/src/PlayerView.jsx
@@ -21,15 +21,15 @@ const PlayerView = props => {
 
   return (
     <Container maxWidth="sm">
-      <h1>2200 Office Hours Queue</h1>
+      <h1>Cal Badminton Open GymQueue</h1>
       {user && (
-        <UserInfo
+        <PlayerInfo
           user={user}
           defaultUser={DEFAULT_USER}
           updateFunction={props.userUpdateFunction}
         />
       )}
-      <StudentList users={users} />
+      <PlayerList users={users} />
       <Box display="flex" flexDirection="row" justifyContent="center">
         <Box p={1}>
           <Button
