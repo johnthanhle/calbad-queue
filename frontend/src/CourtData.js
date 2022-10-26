dddbc84ee3846bd4c5addca6cd798b708eac8987
johnle@berkeley.edu <johnle@berkeley.edu>
2022-10-26 12:54:58 -0700

    better coding practice

--- frontend/src/CourtData.js
+++ frontend/src/CourtData.js
@@ -9,8 +9,9 @@ import { styled } from "@mui/material/styles";
 import PlayerInfo from "./PlayerInfo";
 import { Add, Remove } from "@material-ui/icons";
 
-var sha512 = require("js-sha512");
-var adminHash =
+const sha512 = require("js-sha512");
+
+const adminHash =
   "d47d4b7a1758ca15eec81d5183996da0e94b929d5701acba4781ed8d05b21b9f4bcd0bc30791b375cb665db04b23054d3e65936838968b3370269d2ddbc56667";
 
 const StyledGridOverlay = styled(GridOverlay)(({ theme }) => ({
